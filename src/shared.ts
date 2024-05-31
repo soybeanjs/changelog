fdc46c53dfecd01e319b9164bd6b37e69f9cc825
Soybean <soybeanjs@outlook.com>
2024-05-31 09:53:00 +0800

    chore(projects): update deps & update pnpm version

--- src/shared.ts
+++ src/shared.ts
@@ -3,7 +3,7 @@ import type { Options } from 'execa';
 export async function execCommand(cmd: string, args: string[], options?: Options) {
   const { execa } = await import('execa');
   const res = await execa(cmd, args, options);
-  return res?.stdout?.trim() || '';
+  return (res?.stdout as string)?.trim() || '';
 }
 
 export function notNullish<T>(v?: T | null): v is NonNullable<T> {
