0f572068b2cd2c2e1c8ac1b83e0bd6104922f0e7
honghuangdc <soybeanjs@outlook.com>
2026-05-13 18:14:24 +0800

    chore(tsdown): update tsdown config to use deps for consola

--- packages/githublogen/tsdown.config.ts
+++ packages/githublogen/tsdown.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   dts: false,
   sourcemap: false,
   minify: false,
-  external: ['consola'],
+  deps: {
+    neverBundle: ['consola']
+  },
   fixedExtension: false
 });
